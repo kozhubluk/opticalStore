f6b810177f12bce3b7bcf1088fa62a455e623c5d
Anastasia <nastya.awdeeva@yandex.ru>
2023-05-01 11:52:42 +0300

    ProductService исправлен

--- src/main/java/com/example/opticalStore/services/ProductService.java
+++ src/main/java/com/example/opticalStore/services/ProductService.java
@@ -5,7 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import com.example.opticalStore.repositories.ProductRepository;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -13,6 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+
     public Product getProductById(Long id) {
         return productRepository.findById(id).orElse(null);
     }
@@ -21,12 +24,13 @@ public class ProductService {
         if (title != null) return productRepository.findByTitle(title);
         return productRepository.findAll();
     }
-    public void saveProduct(Product product) {
-        log.info("saving new: {}", product);
-        productRepository.save(product);
-    }
+
+
     public void deleteProduct(Long id) {
         productRepository.deleteById(id);
     }
 
+    public void saveProduct(Product product) {
+        productRepository.save(product);
+    }
 }
